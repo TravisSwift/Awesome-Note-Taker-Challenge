f1f33c7e6a3a3d5367cce1b849ac5341d0360b79
Travis Swift <travis.arthur.swift@gmail.com>
2022-04-27 16:37:16 -0500

    current commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = express();
+const fs = require('fs');
+var jdb = require("db-json");
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
